430a55e331de17637ea913c1d618434dd12eea68
mehrdadmohebi67-web <mehrdadmohebi67@gmail.com>
2026-09-16 03:20:02 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jetbox-latest-v30';
+const CACHE_NAME = 'jetbox-live-v2';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -7,11 +7,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
@@ -20,13 +16,6 @@ self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
     event.respondWith(
       fetch(event.request, { cache: 'no-store' })
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, copy);
-          });
-          return response;
-        })
         .catch(() => caches.match(event.request))
     );
   }
